6ba9cf4d63c26045911e4b6ea958e8ee8914d171
mahesh <shelkemahesh2893@gmail.com>
2023-10-29 14:08:53 +0530

    information about print page

--- test/specs/Protocols/WebDriverProtocols/printPage.js
+++ test/specs/Protocols/WebDriverProtocols/printPage.js
@@ -1,11 +1,13 @@
-const LoginPage = require("../../pageobjects/basePage/login.page");
+const LoginPage = require("../../../pageobjects/basePage/login.page");
 
 describe("browser methods", () => {
   before(async () => {
     await LoginPage.open();
   });
   it("back button in browser", async () => {
-    browser.printPage(scale, background, width, height, top, bottom, left, right, shrinkToFit, pageRanges);
+    //browser.printPage(scale, background, width, height, top, bottom, left, right, shrinkToFit, pageRanges);
+    const printedPage = await browser.printPage();
+    console.log(printedPage); // returns pdf in base 64 format "aW1wb3J0IHsgYnJvd3NlciwgZXhw...." in base64 format you need to decode it to get the pdf
     await browser.pause(4000); // Pause for 2 seconds
   });
 });
