eceb7df4b5954b880c38595e68e34795389e624f
mahesh <shelkemahesh2893@gmail.com>
2023-10-29 13:43:14 +0530

    updating create window method

--- test/specs/Protocols/WebDriverProtocols/createWindow.js
+++ test/specs/Protocols/WebDriverProtocols/createWindow.js
@@ -5,9 +5,11 @@ describe("browser methods", () => {
     await LoginPage.open();
   });
   it("create Window/tab", async () => {
-    await browser.createWindow('tab')
-    console.log((await browser.getWindowHandles()).length) // returns `2`
-    console.log(await browser.getUrl()) // returns "data:,"
+    await browser.createWindow("tab");//used to create a new tab
+    await browser.pause(4000); // Pause for 4 seconds
+    await browser.createWindow("window");//used to create a browser window itself
+    console.log((await browser.getWindowHandles()).length); // returns `2`
+    console.log(await browser.getUrl()); // returns "data:,"
     await browser.pause(4000); // Pause for 4 seconds
   });
 });
